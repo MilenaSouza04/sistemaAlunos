fac57a86d9f15b561e089d1b6ec85514eaeb33a5
MilenaSouza04 <milenabeatrizdesouza@gmail.com>
2025-07-02 21:39:24 -0300

    teste

--- src/test/java/model/AlunoTest.java
+++ src/test/java/model/AlunoTest.java
@@ -12,7 +12,7 @@ class AlunoTest {
 void deveAdicionarEConsultarTreinos() { // Corresponde a "Adicionar Treino Válido" e "Adicionar Múltiplos Treinos"
     Aluno aluno = new Aluno("Milena", "milena@email.com");
     aluno.adicionarTreino("Treino de pernas");
-    aluno.adicionarTreino("Treinos de peito");
+    aluno.adicionarTreino("Treino de peito");
     List<String> treinos = aluno.consultarTreinos();
     assertEquals(2, treinos.size());
     assertTrue(treinos.contains("Treino de pernas"));
